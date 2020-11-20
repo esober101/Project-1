6938d0a4769fe95788989175357c2b494e2b2755
Coaxial <thecoaxial@gmail.com>
2020-11-19 20:56:10 -0500

    Added the Pixabay API and MealDB API and functions to display response

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
-$(document).foundation();
+
+
+var userChoice = 'pizza';
+var testURL = "https://pixabay.com/api/?key=19187965-bb22bcd3a1a38308ab5cb193f&q="+ userChoice +"&image_type=photo&safesearch=true"
+var recipeURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="+userChoice;
+
+
+
+
+  $("#search").click(function() {
+
+    userChoice = $("#inputSearch").val();
+    console.log(userChoice);
+  })
+//image for food item searched by user
+function displayImage(URL,alt) {
+
+  $("#foodPic").attr("src",URL);
+  $("#foodPic").attr("alt",alt);
+
+}
+//info on the food item searched 
+function displayText(header, paragraph) {
+
+  $("#title").append(header);
+  $("#recipeText").append(paragraph);
+
+}
+//grabbing response from pixabay api
+$.ajax({
+  url: testURL,
+  method: "GET"
+}).then(function(response) {
+
+//console.log(response);
+console.log(response);
+
+url = response.hits[0].largeImageURL;
+alt = response.hits[0].tags;
+displayImage(url,alt);
+
+})
+// grabbing response from mealdb api
+$.ajax({
+  url: recipeURL,
+  method: "GET"
+}).then(function(response) {
+  
+//console.log(response);
+console.log(response);
+header = response.meals[0].strMeal;
+paragraph = response.meals[0].strInstructions;
+
+displayText(header, paragraph);
+
+});
+
