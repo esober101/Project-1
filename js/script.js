728f1b31bcf28f13ce5c9a1685e6c862abb0668f
Pavel Darii <paveldarii@yahoo.com>
2020-11-26 14:07:56 -0500

    added Api key to the Calory Ninja Api, and placed the code in the right place

--- js/script.js
+++ js/script.js
@@ -117,6 +117,22 @@ $.ajax({
     displayImage(imageURL, header);
     displayText(header, paragraph);
     displayList(list);
+
+    //Bellow is the code for CaloryCalculator API
+    var query =
+      "Water 150ml,Sugar 1 tsp ,Yeast 15g,Plain Flour 225g,Salt 1 1/2 tsp ,Olive Oil Drizzle,Passata 80g,Mozzarella 70g,Oregano Peeled and Sliced,Basil Leaves,Black Pepper Pinch";
+    $.ajax({
+      method: "GET",
+      url: "https://api.calorieninjas.com/v1/nutrition?query=" + query,
+      headers: { "X-Api-Key": "Wh5+FsEG2j5hRLpM1OAOgA==eqUMFuwC3GqrgzvY" },
+      contentType: "application/json",
+      success: function (result) {
+        console.log(result);
+      },
+      error: function ajaxError(jqXHR) {
+        console.error("Error: ", jqXHR.responseText);
+      },
+    });
   } else {
     //we need to add here what will be displayed on the screen
     // when we will not get an positive response from Api
@@ -124,20 +140,4 @@ $.ajax({
       "This alert happens when user introduces a word that MealDB API cannot find."
     );
   }
-  // <<<<<<< HEAD
-  //   var query = '3lb carrots and a chicken sandwich'
-  //   $.ajax({
-  //     method: 'GET',
-  //     url: 'https://api.calorieninjas.com/v1/nutrition?query=' + query,
-  //     headers: { 'X-Api-Key': 'YOUR_API_KEY'},
-  //     contentType: 'application/json',
-  //     success: function(result) {
-  //         console.log(result);
-  //     },
-  //     error: function ajaxError(jqXHR) {
-  //         console.error('Error: ', jqXHR.responseText);
-  //     }
-  // });
-  // =======
-  // >>>>>>> 953730e2ea693d113f3f59572aa75ee468bb5ac3
 });
